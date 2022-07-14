eadabf4782e2b1eb0f6727ae83ea1ac09d0052c3
msarpong <msarpong1991@gmail.com>
2022-07-14 18:33:39 +0200

    ADD splashscreen activity

---
+++ app/src/main/java/com/msarpong/giphy/ui/activity/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.msarpong.giphy.ui.activity
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.msarpong.giphy.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
